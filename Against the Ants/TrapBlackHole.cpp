9a2ab78d579ffd96fd9416239b93e57485ba70c9
Dau Duc Hieu <84106812+DauDucHieu@users.noreply.github.com>
2024-05-14 20:58:58 +0700

    Complete game - last commit for v1

--- Against the Ants/TrapBlackHole.cpp
+++ Against the Ants/TrapBlackHole.cpp
@@ -2,6 +2,8 @@
 
 TrapBlackHole::TrapBlackHole(double x, double y, double radius) : Trap(x, y, radius) {
 	this->spriteSrc = "resources/sprites/black-hole.png";
+	this->hp /= 10;
+	this->currentHp = this->hp;
 }
 
 void TrapBlackHole::EffectToAnt(Ant* ant) {
